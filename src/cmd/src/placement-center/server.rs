d50e209d9a4a5e0a9164cf596255d6d299976979
Maku319 <bancangbaize@gmail.com>
2024-09-26 15:47:45 +0800

    ✨ feat : add cli for read config.

--- src/cmd/src/placement-center/server.rs
+++ src/cmd/src/placement-center/server.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+pub const DEFAULT_PLACEMENT_CENTER_CONFIG: &str = "config/placement-center.toml";
+
+#[derive(Debug, Parser)]
+#[command(author="Maku319", version="0.0.1", about="RobustMQ: Next generation high-performance message queue.", long_about = None)]
+#[command(next_line_help = true)]
+struct ArgsParams {
+    #[arg(short, long, default_value_t=String::from(DEFAULT_PLACEMENT_CENTER_CONFIG))]
+    conf: String,
+}
+
 fn main() {
-    println!("Get started");
+    let args = ArgsParams::parse();
+    println!("conf path: {:?}", args.conf);
 }
